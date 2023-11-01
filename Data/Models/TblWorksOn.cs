284a2823512f84c0ac1b4c2f4685c8c5582996df
GiangNNT <giangnntse173469@fpt.edu.vn>
2023-11-01 14:25:24 +0700

    Project Repo

---
+++ Data/Models/TblWorksOn.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Data.Models
+{
+    public partial class TblWorksOn
+    {
+        public decimal EmpSsn { get; set; }
+        public int ProNum { get; set; }
+        public int? WorkHours { get; set; }
+
+        public virtual TblProject ProNumNavigation { get; set; }
+    }
+}
---
+++ Data/Repository/ProjectRepository.cs
@@ -0,0 +1,30 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repository
+{
+    public class ProjectRepository : RepositoryBase<TblProject>
+    {
+        private readonly FUH_COMPANYContext _dbContext;
+        public ProjectRepository(FUH_COMPANYContext dcContext)
+        {
+            _dbContext = dcContext;
+        }
+        public ProjectRepository getAll()
+        {
+            return _dbContext.TblProjects.ToList();
+        }
+        public ProjectRepository getById(int id)
+        {
+            return _dbContext.TblProjects.FirstOrDefault(p => p.Id == id);
+        }
+        public int deleteProject(int id)
+        {
+            _dbContext.TblProjects.Remove(p => p.Id == id);
+        }
+    }
+}
