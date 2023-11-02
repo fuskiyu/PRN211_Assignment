325861c9e015d257cb3e2ce0b0f54fd0ffd9508c
GiangNNT <giangnntse173469@fpt.edu.vn>
2023-11-03 03:12:56 +0700

    Initial commit

---
+++ Data/Repository/ProjectRepository.cs
@@ -0,0 +1,45 @@
+﻿using Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repository
+{
+    public class ProjectRepository : RepositoryBase<TblProject>
+    {
+        private readonly FUH_COMPANYContext _dbContext;
+        public ProjectRepository(FUH_COMPANYContext dcContext)
+        {
+            _dbContext = dcContext;
+        }
+        public List<TblProject> getAll()
+        {
+            var project = _dbContext.TblProjects
+                .Include(p => p.DepNumNavigation)
+                .Include(p => p.LocNumNavigation)
+                .Select(p => new TblProject
+                {
+                    ProNum = p.ProNum,
+                    ProName = p.ProName,
+                    DepNum = p.DepNum,
+                    LocNum = p.LocNum,
+                    DepNumNavigation = p.DepNumNavigation,
+                    LocNumNavigation = p.LocNumNavigation,
+                }).ToList();
+            return project;
+        }
+        public TblProject getById(int id)
+        {
+            return _dbContext.TblProjects.FirstOrDefault(p => p.ProNum == id) ?? throw new Exception("Project not found");
+        }
+        public int deleteProject(int id)
+        {
+            var delProject = _dbContext.TblProjects.Find(id) ?? throw new Exception("Project not found");
+            _dbContext.TblProjects.Remove(delProject);
+            return _dbContext.SaveChanges();
+        }
+    }
+}
---
+++ Presentation/Management/ProjectManagement.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Presentation.Management
+{
+    partial class ProjectManagement
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(548, 12);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(569, 570);
+            dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(36, 45);
+            label1.Name = "label1";
+            label1.Size = new Size(126, 28);
+            label1.TabIndex = 1;
+            label1.Text = "Project name";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(36, 113);
+            label2.Name = "label2";
+            label2.Size = new Size(183, 28);
+            label2.TabIndex = 2;
+            label2.Text = "Project Department";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(36, 184);
+            label3.Name = "label3";
+            label3.Size = new Size(153, 28);
+            label3.TabIndex = 3;
+            label3.Text = "Project Location";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(306, 53);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(153, 23);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(306, 121);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(153, 23);
+            textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(306, 189);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(153, 23);
+            textBox3.TabIndex = 6;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(391, 314);
+            button1.Name = "button1";
+            button1.Size = new Size(87, 39);
+            button1.TabIndex = 7;
+            button1.Text = "Add";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // ProjectManagement
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1129, 594);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(dataGridView1);
+            Name = "ProjectManagement";
+            Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button1;
+    }
+}
